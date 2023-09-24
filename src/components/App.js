8115af36a46113ad3c04c72a9f7b2df10abe00e2
Anna Raptidou Cole <acrrj123@gmail.com>
2023-09-24 13:37:16 -0500

    add category filter buttons and render list of tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+//console.log("Here's the data you're working with");
+//console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [categories, setCategories] = useState(CATEGORIES)
+
+  function handleDelete(text) {
+    const updatedList = tasks.filter(task => task.text !== text)
+    setTasks(updatedList)
+  }
+ 
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={categories}/>
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={tasks} onClickDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map(catFilter => 
+      <button key={catFilter}>{catFilter}</button>)}
     </div>
   );
 }
